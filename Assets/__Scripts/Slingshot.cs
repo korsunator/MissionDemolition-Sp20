d572666bb9542aef4343d9387248894b363a1250
korsunator <35782970+korsunator@users.noreply.github.com>
2020-02-13 17:01:45 -0500

    added mission demolition c# script

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -88,6 +88,9 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 }
